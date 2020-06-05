e50c680e110b80cb605e1b9eeef624bf8c068839
Litvinenko <ayloolya9@gmail.com>
2020-06-05 05:22:31 +0300

    constants

---
+++ Arkanoid/src/com/example/Const.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Const {
+    public static final int SCREEN_WIDTH = 800; //нижняя граница
+    public static final int SCREEN_HEIGHT = 600; //верхняя граница
+    public static final double BLOCK_WIDTH = 60.0; //ширина блока
+    public static final double BLOCK_HEIGHT = 20.0; //высота блока
+    public static final int COUNT_BLOCKS_X = 11; //количество блоков в строке
+    public static final int COUNT_BLOCKS_Y = 4; //количество блоков в столбце
+    public static final double PADDLE_WIDTH = 80.0; //ширина платформы
+    public static final double PADDLE_HEIGHT = 20.0; //высота платформы
+    public static final double PADDLE_VELOCITY = 0.6; //скорость движения платформы
+    public static final double BALL_RADIUS = 10.0; //радиус шарика
+    public static final double BALL_VELOCITY = 0.3; //скорость движения шарика
+    public static final int PLAYER_LIVES = 5; //количество жизней
+    public static final double FT_SLICE = 1.0; //ФУТОВЫЙ КУСОК
+    public static final double FT_STEP = 1.0; //ФУТОВЫЙ ШАГ
+}
